e33fe2d7aa6b004abbe45ac5e6f4e37a48eac333
Trent Townsend <trent.townsend2@gmail.com>
2022-12-22 16:06:48 +1000

    Add ability to track numerical score on click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const ratingButtons = document.querySelectorAll(".rate-button");
 console.log(ratingButtons);
+const scores = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
 
 function selectRating() {
   event.preventDefault();
@@ -11,6 +12,7 @@ function selectRating() {
   }
   this.classList.add("rating-selected");
   this.classList.remove("rate-button-inactive");
+  let rating = scores[this.id]
 }
 
 for (let i = 0; i < ratingButtons.length; i++) {
